7fe417220ca5f2734beffc3568dd83e9414898da
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-06-06 20:01:00 +0530

    config file of drizzle mod

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
     out: './drizzle',
-    schema: './src/models/user.schema.js',
+    schema: './src/models/index.js',
     dialect: 'postgresql',
     dbCredentials: {
         url: "postgres://postgres:parvesh@localhost:5432/bitly",
