cb95cbc93a8c59bcbeb874bbbd61123f5c608e33
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-06-07 10:57:11 +0530

    fixed bug in auth middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,9 +1,17 @@
 
+import jwt from "jsonwebtoken";
 
 export const userAuthentication = (req, res, next)=>{
 
     try {
-        let token = req.header("Authorization")?.replace("Bearer","");
+        let token = req.header("Authorization")?.replace("Bearer","").trim();
+
+        // if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        //     return res.status(401).json({
+        //         message: "Unauthorized User",
+        //         success: false
+        //     });
+        // }
 
         if(!token){
             return res.status(401).json({
@@ -14,6 +22,7 @@ export const userAuthentication = (req, res, next)=>{
         }
 
         let decode = jwt.verify(token,"superman");
+      
         req.user = decode;
         next();
         
@@ -21,7 +30,7 @@ export const userAuthentication = (req, res, next)=>{
         
         console.log(  "error 123" , error)
         return res.status(401).json({
-            message: "Invalid or Expired Session",
+            message: "Invalid or Expired Token",
             success: false
         });
     }
